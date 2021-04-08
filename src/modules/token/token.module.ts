e7eaaba5abcc0ff352309e25290afd3b869135cd
IanTorquato <iantorquato2@gmail.com>
2021-04-08 17:02:48 -0300

    Removing unused importations

--- src/modules/token/token.module.ts
+++ src/modules/token/token.module.ts
@@ -2,13 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { DatabaseModule } from 'src/database/database.module';
-import { UserModule } from 'src/modules/user/user.module';
 import { TokenController } from './token.controller';
 import { tokenProvider } from './token.provider';
 import { TokenService } from './token.service';
 
 @Module({
-  imports: [DatabaseModule, forwardRef(() => AuthModule), UserModule],
+  imports: [DatabaseModule, forwardRef(() => AuthModule)],
   providers: [...tokenProvider, TokenService],
   exports: [TokenService],
   controllers: [TokenController],
